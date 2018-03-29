efed53b8c8dfc5cbb4b27d14951e96ec4c799e40
Parang <pdahal1@users.noreply.github.com>
2018-03-29 13:07:58 -0400

    Update Helloworld.java

--- src/hi/Helloworld.java
+++ src/hi/Helloworld.java
@@ -2,6 +2,20 @@ package hi;
 
 public class Helloworld {
 public static void main(String[] args) {
+	Scanner Sc= new Scanner (System.in); 
+	
 	System.out.println("Hello world");
+	System.out.println("Enter your username");
+	String UserName= Sc.nextLine(); 
+	if ( UserName= "Param"){
+	Password (); 
+	}
+	
 }
+	public static void Password(){
+	System.out.println("Please enter your password");
+		String Password = Sc.nextLine(); 
+		if ( password.length()>6){
+		System.out.println("your password matches. ")}
+	}
 }
